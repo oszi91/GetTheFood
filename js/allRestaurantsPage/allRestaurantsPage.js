00634ac0d6fd0e05f4056f4708221319ee50c0e3
oszywapawel <pawel.oszywa@wp.pl>
2021-03-14 21:05:12 +0100

    add filters template

--- js/allRestaurantsPage/allRestaurantsPage.js
+++ js/allRestaurantsPage/allRestaurantsPage.js
@@ -11,11 +11,10 @@ class allRestaurantsPage extends Component {
                 </div>
                 <div className="container">
                     <div className="allRestaurants__container">
-
                         <div className="allRestaurants__list">
                             <div className="allRestaurants__list__search">
                                 <input type="text" />
-                                <label htmlFor="sort">Choose a car:</label>
+                                <label htmlFor="sort">Sort by:</label>
                                 <select name="sort">
                                     <option value="popular">Popular</option>
                                     <option value="min_order_amount">Min. order amount</option>
@@ -29,7 +28,63 @@ class allRestaurantsPage extends Component {
                         </div>
 
                         <div className="allFood__filters">
-
+                            <div className="freeDelivery">
+                                <p className="freeDelivery__text">Free delivery </p>
+                                <div className="freeDelivery__btn">
+                                    <label className="switch">
+                                        <input className="switch__input" type="checkbox" />
+                                        <span className="slider"></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="timeDelivery">
+                                <form className="timeDelivery__form">
+                                    <p className="timeDelivery__form__text">Delivery time</p>
+                                    <div className="timeDelivery__form__radio">
+                                        <input type="radio" id="time_show_all" name="time"
+                                            value="time_show_all" />
+                                        <label htmlFor="time_show_all"> Show All</label>
+                                    </div>
+                                    <div className="timeDelivery__form__radio">
+                                        <input type="radio" id="under30min" name="time"
+                                            value="under30min" />
+                                        <label htmlFor="under30min"> less than 30 min</label>
+                                    </div>
+                                    <div className="timeDelivery__form__radio">
+                                        <input type="radio" id="under1h" name="time"
+                                            value="under1h" />
+                                        <label htmlFor="under1h"> Under 1 hour</label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="minPriceDelivery">
+                                <form className="minPriceDelivery__form">
+                                    <p className="minPriceDelivery__form__text">Min. cost of delivery</p>
+                                    <div className="minPriceDelivery__form__radio">
+                                        <input type="radio" id="price_show_all" name="price"
+                                            value="price_show_all" />
+                                        <label htmlFor="price_show_all"> Show all</label>
+                                    </div>
+                                    <div className="minPriceDelivery__form__radio">
+                                        <input type="radio" id="under30PLN" name="price"
+                                            value="under30PLN" />
+                                        <label htmlFor="under30PLN"> less than 30 PLN</label>
+                                    </div>
+                                    <div className="minPriceDelivery__form__radio">
+                                        <input type="radio" id="under60PLN" name="price"
+                                            value="under60PLN" />
+                                        <label htmlFor="under60PLN"> less than 60 PLN</label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="deliveryRating">
+                                <p className="deliveryRating__text">Restaurant rating</p>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
+                                <span className="fa fa-star"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
